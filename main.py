ae96f65aa23675dfc3dd7d3566f799d0d516df8c
ASUS <2442616509@qq.com>
2025-06-20 16:59:39 +0800

    优化部分内容

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ from tts.sovits import text_to_voice
 from audio.play_audio import play_voice
 
 if __name__ == "__main__":
+    i = 0
     while True:
         #开始录音
         record_voice("input.wav", duration=5)
-        print("录音结束")
+        print(f"第{i}次录音结束")
         #录音存储
         user_text = recognize_audio("input.wav")
 
@@ -21,3 +22,4 @@ if __name__ == "__main__":
 
         #播放语音
         play_voice("output.wav")
+        i+=1
